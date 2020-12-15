da9d8f80160e84de592c58edaba459ad931923b4
mthaw <mthaw@10.5.1.79>
2020-12-15 14:22:43 -0500

    Ajouter les commentaires

--- MartinSommatif2/src/SommePremiers.java
+++ MartinSommatif2/src/SommePremiers.java
@@ -8,35 +8,44 @@
 import java.util.*;
 
 public class SommePremiers {
-
+/**
+ * 
+ * @param tableau, le tableau d'entiers dans lequel on veut trouver la somme des nombres premiers.
+ * @return la somme des nombres premiers dans tableau
+ */
 	public static int sommePremiers(int tableau[]) {
-		int somme = 0;
-		for (int i = 0; i < tableau.length; i++) {
-			if (isPremier(tableau[i])) {
-				somme += tableau[i];
+		int somme = 0;//Creer la variable somme
+		for (int i = 0; i < tableau.length; i++) {//Pour chaque valeur dans tableau
+			if (isPremier(tableau[i])) {//Si il est premier
+				somme += tableau[i];//Augmenter la somme par ce nombre premier
 			}
 		}
-		return somme;
+		return somme;//Retourner la somme des nombres premiers dans tableau
 	}
-
+/**
+ * 
+ * @param valeur, on veut verifier si valeur est un nombre premier
+ * @return true, si valeur est un nombre premier et false sinon (valeur n'est pas un nombre premier)
+ */
 	public static boolean isPremier(int valeur) {
-		if (valeur == 1) {
+		if (valeur == 1) {//Si valeur est 1, il n'est pas premier (exception)
 			return false;
 		}
-		for (int i = 2; i <= valeur / 2; i++) {
-			if (valeur % i == 0) {
-				return false;
+		for (int i = 2; i <= valeur / 2; i++) {//Verifier tous les facteurs jusqu'a un demi de valeur (=valeur/2)
+			if (valeur % i == 0) {//Si il est divisible par ce facteur i, alors 
+				return false;//il n'est pas premier
 			}
 
 		}
-		return true;
+		return true;//Si aucun facteur le divise (la methode n'a pas deja fait return), valeur est premier
 	}
 
 	public static void main(String[] args) {
-		int monTableau[] = new int[100];
-		for (int i = 0; i < 100; i++) {
-			monTableau[i] = (int) (Math.random() * 100000);
+		int monTableau[] = new int[100];//Creer le tableau de taille 100
+		for (int i = 0; i < 100; i++) {//Pour chaque element du tableau
+			monTableau[i] = (int) (Math.random() * 100000);//Mettre un nombre au hazard entre 1 et 99999 inclusive
 		}
+		//Appeler la methode somme Premiers et afficher le resultat:
 		System.out.println(sommePremiers(monTableau));
 	}
 
