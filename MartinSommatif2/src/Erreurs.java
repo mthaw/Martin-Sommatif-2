9332c40dc9dd9bd54b1028319c2d402c2bf05f2f
mthaw <mthaw@10.5.1.79>
2020-12-15 14:04:12 -0500

    Erreur de francais

--- MartinSommatif2/src/Erreurs.java
+++ MartinSommatif2/src/Erreurs.java
@@ -16,12 +16,13 @@
  * 
  * 4. La methode saluerAmi devrait retourner un String (comme specifie dans sa definition sur la ligne 20), mais elle ne le fait pas. Il faut retourner le message
  * 
- * 5. La methode saluerAmi n'est pas static. Il faut etre statique pour etre appele dans le main (qui est statique).
+ * 5. La methode saluerAmi n'est pas static. Il faut etre static pour etre appele dans le main (qui est static).
  */
 public class Erreurs {
-	public static void main(String []args) {
+	public static void main(String[] args) {
 		System.out.println(saluerAmi("John"));
 	}
+
 	public static String saluerAmi(String nom) {
 		String message = "Bonjour mon ami " + nom;
 		return message;
