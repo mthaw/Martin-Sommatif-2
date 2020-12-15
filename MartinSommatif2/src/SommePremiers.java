90b537b475fb265165116ac834237be8e22b3939
mthaw <mthaw@10.5.1.79>
2020-12-15 14:38:37 -0500

    Formattage et version finale

--- MartinSommatif2/src/SommePremiers.java
+++ MartinSommatif2/src/SommePremiers.java
@@ -6,44 +6,47 @@
 */
 
 public class SommePremiers {
-/**
- * 
- * @param tableau, le tableau d'entiers dans lequel on veut trouver la somme des nombres premiers.
- * @return la somme des nombres premiers dans tableau
- */
+	/**
+	 * 
+	 * @param tableau, le tableau d'entiers dans lequel on veut trouver la somme des
+	 *                 nombres premiers.
+	 * @return la somme des nombres premiers dans tableau
+	 */
 	public static int sommePremiers(int tableau[]) {
-		int somme = 0;//Creer la variable somme, la somme des nombres premiers dans tableau
-		for (int i = 0; i < tableau.length; i++) {//Pour chaque valeur dans tableau
-			if (isPremier(tableau[i])) {//Si il est premier
-				somme += tableau[i];//Augmenter la somme par ce nombre premier
+		int somme = 0;// Creer la variable somme, la somme des nombres premiers dans tableau
+		for (int i = 0; i < tableau.length; i++) {// Pour chaque valeur dans tableau
+			if (isPremier(tableau[i])) {// Si il est premier
+				somme += tableau[i];// Augmenter la somme par ce nombre premier
 			}
 		}
-		return somme;//Retourner la somme des nombres premiers dans tableau
+		return somme;// Retourner la somme des nombres premiers dans tableau
 	}
-/**
- * 
- * @param valeur, on veut verifier si valeur est un nombre premier
- * @return true, si valeur est un nombre premier et false sinon (valeur n'est pas un nombre premier)
- */
+
+	/**
+	 * 
+	 * @param valeur, on veut verifier si valeur est un nombre premier
+	 * @return true, si valeur est un nombre premier et false sinon (valeur n'est
+	 *         pas un nombre premier)
+	 */
 	public static boolean isPremier(int valeur) {
-		if (valeur == 1) {//Si valeur est 1, 
-			return false;//il n'est pas premier (exception)
+		if (valeur == 1) {// Si valeur est 1,
+			return false;// il n'est pas premier (exception)
 		}
-		for (int i = 2; i <= valeur / 2; i++) {//Verifier tous les facteurs de 2 jusqu'a un demi de valeur (=valeur/2)
-			if (valeur % i == 0) {//Si il est divisible par ce facteur i (donne un reste de 0), alors 
-				return false;//il n'est pas premier
+		for (int i = 2; i <= valeur / 2; i++) {// Verifier tous les facteurs de 2 jusqu'a un demi de valeur (=valeur/2)
+			if (valeur % i == 0) {// Si il est divisible par ce facteur i (donne un reste de 0), alors
+				return false;// il n'est pas premier
 			}
 
 		}
-		return true;//Si aucun facteur le divise (la methode n'a pas deja fait return), valeur est premier
+		return true;// Si aucun facteur le divise (la methode n'a pas deja fait return), valeur est premier
 	}
 
 	public static void main(String[] args) {
-		int monTableau[] = new int[100];//Creer le tableau de taille 100
-		for (int i = 0; i < 100; i++) {//Pour chaque element du tableau
-			monTableau[i] = (int) (Math.random() * 100000);//Mettre un nombre au hazard entre 1 et 99999 inclusive
+		int monTableau[] = new int[100];// Creer le tableau de taille 100
+		for (int i = 0; i < 100; i++) {// Pour chaque element du tableau
+			monTableau[i] = (int) (Math.random() * 100000);// Mettre un nombre au hazard entre 1 et 99999 inclusive
 		}
-		//Appeler la methode somme Premiers et afficher le resultat:
+		// Appeler la methode somme Premiers et afficher le resultat:
 		System.out.println(sommePremiers(monTableau));
 	}
 
